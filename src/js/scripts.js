4ce74bb6ff425e45f19ada130f47d5253d105f7b
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-15 19:31:14 -0300

    logic of result complete

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,11 +9,30 @@ const listResultNum = document.getElementById('result')
 const areaResult = document.getElementById('area-result')
 const areaNumberDraw = document.getElementById('area-number-draw')
 
-
+let resultNumbersDraw = []
 
 inputTratament(numbers)
 inputTratament(numberMin)
 inputTratament(numberMax)
+
+function createElement({ resultNumbersDraw, numberForRepeat, min, max }){
+    if(numberForRepeat > 0){
+        for(let i = 0; i <= numberForRepeat ?? 0; i++){
+            resultNumbersDraw.push(randomNumberInterval(min ?? null, max ?? null))
+        }
+    }
+
+    for (const element of resultNumbersDraw){
+        const li = document.createElement('li')
+        const span = document.createElement('span')
+
+        span.textContent = element
+        li.append(span)
+        listResultNum.append(li)
+    }
+
+    resultNumbersDraw = []
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -22,13 +41,9 @@ form.addEventListener('submit', (e) => {
     const max = Number(numberMax.value) + 1
     const numberForRepeat = Number(numbers.value - 1)
 
-    for(let i = 0; i <= numberForRepeat; i++){
-        const li = document.createElement('li')
-        const span = document.createElement('span')
-        span.textContent = randomNumberInterval(min, max)
-        li.append(span)
-        listResultNum.append(li)
-    }
+    createElement({ resultNumbersDraw, numberForRepeat, min, max })
+
+    resultNumbersDraw = []
 
     setTimeout(() => {
         areaNumberDraw.style.opacity = '0'
@@ -43,22 +58,24 @@ form.addEventListener('submit', (e) => {
 })
 
 returnBtn.addEventListener('click', () => {
-    for (let i = 0; i < listResultNum.childElementCount; i++) {
-        listResultNum.removeChild(i)
-        console.log(listResultNum)
-    }
-
     setTimeout(() => {
         areaResult.style.opacity = '0'
         setTimeout(() => {
             areaResult.style.display = 'none'
             areaNumberDraw.style.display = 'flex'
 
+            const listResultChildren = Array.from(listResultNum.children)
+
+            for (const item of listResultChildren) {
+                listResultNum.removeChild(item)
+            }
+
             setTimeout(() => {
                 areaNumberDraw.style.opacity = '1'
             }, 10)
         }, 500)
     }, 500)
+
 })
 
 // functions
